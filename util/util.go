2c56bc2fc925f8fdc12ba62c38b4567b096091a5
Oribe <d_egoist@163.com>
2020-09-10 18:34:51 +0800

    添加转换方法

--- util/util.go
+++ util/util.go
@@ -1,2 +1,19 @@
 package util
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+// SwitchSqlNull 转换
+func SwitchSqlNull(sqlNull interface{}) error {
+	t := reflect.TypeOf(sqlNull)
+	if t.Kind() != reflect.Struct {
+		// err := errors.New("must be pass a struct, but get a %v", t.Kind())
+		err := errors.New("must be pass a struct, but get a %v")
+		return err
+	}
+	fmt.Println(sqlNull)
+	return nil
+}
